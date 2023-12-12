8da92c3aa93c0efb2e83697f78dba62be93fe02d
Diego Ayala <dieguito9677@gmail.com>
2023-12-12 16:59:13 -0400

    gestion proyecto finish

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ProyectoListView, asigancion_materiales, reportes, ProyectoCreateView
+from .views import ProyectoListView, asigancion_materiales, reportes, ProyectoCreateView, ProyectoUpdateView, ProyectoDeleteView
 
 urlpatterns = [
     path('', ProyectoListView.as_view(), name='proyectos'),
     path('create/', ProyectoCreateView.as_view(), name='proyecto-create'),
-    path('update/<int:pk>/', ProyectoCreateView.as_view(), name='proyecto-update'),
-    path('delete/<int:pk>/', ProyectoCreateView.as_view(), name='proyecto-delete'),
+    path('update/<int:pk>/', ProyectoUpdateView.as_view(), name='proyecto-update'),
+    path('delete/<int:pk>/', ProyectoDeleteView.as_view(), name='proyecto-delete'),
     path('materiales/', asigancion_materiales, name='materiales'),
     path('reportes/', reportes, name='reportes'),
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -3,9 +3,9 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, FormView, DeleteView
+from django.views.generic import ListView, DeleteView, FormView
 
-from .models import Proyecto
+from .models import Proyecto, Barrio
 from .forms import ProyectoForm
 
 
@@ -18,13 +18,65 @@ class ProyectoListView(LoginRequiredMixin, ListView):
         return qs
 
 
-class ProyectoCreateView(FormView):
+class ProyectoCreateView(LoginRequiredMixin, FormView):
     template_name = 'proyecto_form.html'
     form_class = ProyectoForm
     success_url = '/proyecto/'
 
-    def form_valid(self, form: Any) -> HttpResponse:
+    def form_valid(self, form: ProyectoForm) -> HttpResponse:
         print(form.cleaned_data)
+        barrio = Barrio.objects.create(
+            nombre=form.cleaned_data['barrio'],
+            poblacion=form.cleaned_data['poblacion'],
+            area=form.cleaned_data['area'],
+        )
+        Proyecto.objects.create(
+            tipo=form.cleaned_data['tipo'],
+            nombre=form.cleaned_data['nombre'],
+            fecha_inicio=form.cleaned_data['fecha_inicio'],
+            fecha_fin=form.cleaned_data['fecha_fin'],
+            barrio=barrio,
+            estado=1,
+        )
+
+        return super().form_valid(form)
+
+
+class ProyectoUpdateView(LoginRequiredMixin, FormView):
+    template_name = 'proyecto_form.html'
+    form_class = ProyectoForm
+    success_url = '/proyecto/'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['object'] = Proyecto.objects.select_related('tipo', 'barrio').get(pk=self.kwargs['pk'])
+        return context
+
+    def get_initial(self) -> dict[str, Any]:
+        initial = super().get_initial()
+        obj = Proyecto.objects.select_related('tipo', 'barrio').get(pk=self.kwargs['pk'])
+        initial.update({
+            'tipo': obj.tipo,
+            'nombre': obj.nombre,
+            'barrio': obj.barrio.nombre,
+            'poblacion': obj.barrio.poblacion,
+            'area': obj.barrio.area,
+            'fecha_inicio': obj.fecha_inicio,
+            'fecha_fin': obj.fecha_fin,
+        })
+        return initial
+
+    def form_valid(self, form: ProyectoForm) -> HttpResponse:
+        obj = Proyecto.objects.select_related('tipo', 'barrio').get(pk=self.kwargs['pk'])
+        obj.tipo = form.cleaned_data['tipo']
+        obj.nombre = form.cleaned_data['nombre']
+        obj.barrio.nombre = form.cleaned_data['barrio']
+        obj.barrio.poblacion = form.cleaned_data['poblacion']
+        obj.barrio.area = form.cleaned_data['area']
+        obj.fecha_inicio = form.cleaned_data['fecha_inicio']
+        obj.fecha_fin = form.cleaned_data['fecha_fin']
+        obj.barrio.save()
+        obj.save()
         return super().form_valid(form)
 
 
